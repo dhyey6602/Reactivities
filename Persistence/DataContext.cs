60f00890975cc6343a58e49f411423093d5f3dc3
dhyey6602 <dhyey6602patel@gmail.com>
2021-08-11 16:58:54 +0530

    Access specifier updated

--- Persistence/DataContext.cs
+++ Persistence/DataContext.cs
@@ -10,6 +10,6 @@ namespace Persistence
         {
 
         }
-        public DbSet<Activity> Activities { get; set; }
+        public DbSet<Activity> Activities { get; set; } // default getter , setter
     }
 }
